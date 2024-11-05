b9c6c231f3ffe12bd5477740043fcc95ced6e79d
meongju0o0 <bl4angel20@khu.ac.kr>
2024-11-05 20:23:17 +0900

    .

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ class blogImage(viewsets.ModelViewSet):
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
