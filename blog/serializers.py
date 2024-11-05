febcf37be117b5c17f9ff28615f67f1bfa37f6bb
meongju0o0 <bl4angel20@khu.ac.kr>
2024-11-05 20:28:08 +0900

    .

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,8 +7,8 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
     
     class Meta:
         model = Post
-        fields = ('author', 'title', 'text', 'image')
-    
+        fields = ['author', 'title', 'text', 'created_date', 'published_date', 'image']
+        
     def create(self, validated_data):
         print("Received Data:", validated_data)
         
